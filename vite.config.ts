6d872e1e59845797dd07c7fe847762fe71d97b4b
Liangxiao <liangxiao0991@gmail.com>
2024-04-17 00:12:10 +0800

    add mock data

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { viteMockServe } from 'vite-plugin-mock'
 import { vitePluginForArco } from '@arco-plugins/vite-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteMockServe({}),
     VueDevTools(),
     vitePluginForArco({
       style: 'css'

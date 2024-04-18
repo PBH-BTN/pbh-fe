5db8055c17352dd5a8ed23aad0b2cc5ccbe1e680
Gaojianli <admin@gaojianli.me>
2024-04-18 14:06:53 +0800

    add autoswitch bottun

--- src/views/LogView.vue
+++ src/views/LogView.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import banlogTable from '../components/banlogTable.vue'
+import { ref } from 'vue'
+const lastUpdate = new Date().toLocaleString()
+const autoUpdate = ref(true)
 </script>
 
 <template>
+  <p style="font-size: 1.2em">此页面展示 PeerBanHelper 储存的封禁历史记录信息。</p>
+  <a-space size="large">
+    <p><icon-clock-circle />最后更新于：{{ lastUpdate }}</p>
+    <a-switch v-model="autoUpdate">
+      <template #checked> 自动更新开 </template>
+      <template #unchecked> 自动更新关 </template>
+    </a-switch>
+  </a-space>
   <banlogTable />
 </template>

cc6ac37de639eda7726638d9325bfa32edaad391
Gaojianli <admin@gaojianli.me>
2024-04-19 16:35:06 +0800

    增加骨架屏

--- src/components/clientStatus.vue
+++ src/components/clientStatus.vue
@@ -1,12 +1,17 @@
 <template>
-  <!-- <a-skeleton :animation="true">
-    <a-space direction="vertical" :style="{ width: '100%' }" size="large">
-      <a-skeleton-line :rows="3" />
-      <a-skeleton-shape />
-    </a-space>
-  </a-skeleton> -->
   <a-row justify="space-around">
-    <a-col :span="4" v-for="client in data" :key="client.name">
+    <a-col v-if="!firstLoad" :span="4">
+      <a-card :style="{ width: '360px' }" hoverable>
+        <a-skeleton :animation="true">
+          <a-space direction="vertical" :style="{ width: '100%' }" size="large">
+            <a-skeleton-line />
+            <a-skeleton-line />
+            <a-skeleton-line />
+            <a-skeleton-line />
+          </a-space>
+        </a-skeleton> </a-card
+    ></a-col>
+    <a-col v-else :span="4" v-for="client in data" :key="client.name">
       <a-card :style="{ width: '360px' }" title="下载器状态" hoverable>
         <a-typography>
           <a-tooltip :content="client.endpoint">
@@ -15,10 +20,14 @@
             </a-typography-paragraph>
           </a-tooltip>
           <a-typography-paragraph>
-            状态： <component :is="statusMap.get(client.status)!![2]"></component
-            ><a-typography-text :type="statusMap.get(client.status)!![0]">{{
-              statusMap.get(client.status)!![1]
-            }}</a-typography-text>
+            状态：
+            <a-typography-text :type="statusMap.get(client.status)!![0]"
+              ><icon-check-circle-fill v-if="client.status == ClientStatus.HEALTHY" />
+              <icon-close-circle-fill v-if="client.status == ClientStatus.ERROR" />
+              <icon-exclamation-circle-fill v-if="client.status == ClientStatus.UNKNOWN" />{{
+                statusMap.get(client.status)!![1]
+              }}</a-typography-text
+            >
           </a-typography-paragraph>
 
           <a-typography-paragraph> 活动种子数: {{ client.torrents }}</a-typography-paragraph>
@@ -34,23 +43,21 @@ import { getClientStatus } from '@/service/clientStatus'
 import { ClientStatus } from '@/api/model/clientStatus'
 import { useAutoUpdate } from '@/stores/autoUpdate'
 import { useRequest } from 'vue-request'
-import { watch } from 'vue'
+import { watch, ref } from 'vue'
 const statusMap = new Map([
-  [ClientStatus.HEALTHY, ['success', '正常 - 状态良好', 'icon-check-circle-fill']],
-  [
-    ClientStatus.ERROR,
-    ['warning', '错误 - 与客户端通信时出错，请检查日志文件', 'icon-close-circle-fill']
-  ],
-  [
-    ClientStatus.UNKNOWN,
-    ['danger', '未知 - PeerBanHelper 可能还没有与此客户端通信', 'icon-exclamation-circle-fill']
-  ]
+  [ClientStatus.HEALTHY, ['success', '正常 - 状态良好']],
+  [ClientStatus.ERROR, ['warning', '错误 - 与客户端通信时出错，请检查日志文件']],
+  [ClientStatus.UNKNOWN, ['danger', '未知 - PeerBanHelper 可能还没有与此客户端通信']]
 ])
+const firstLoad = ref(false)
 const autoUpdateState = useAutoUpdate()
-const { data, loading, run, cancel } = useRequest(getClientStatus, {
+const { data, run, cancel } = useRequest(getClientStatus, {
   pollingWhenOffline: true,
   pollingInterval: 3000,
-  onSuccess: () => autoUpdateState.setLastUpdate(new Date())
+  onSuccess: () => {
+    firstLoad.value = true
+    autoUpdateState.setLastUpdate(new Date())
+  }
 })
 watch(autoUpdateState, (state) => {
   if (state.autoUpdate) {
--- src/service/clientStatus.ts
+++ src/service/clientStatus.ts
@@ -1,6 +1,6 @@
 import type { clientStatus } from '@/api/model/clientStatus'
 
 export function getClientStatus(): Promise<clientStatus[]> {
-  const url = new URL('api/clientStatus', location.href)
+  const url = new URL(import.meta.env.VITE_APP_BASE_URL + 'api/clientStatus', location.href)
   return fetch(url).then((res) => res.json())
 }

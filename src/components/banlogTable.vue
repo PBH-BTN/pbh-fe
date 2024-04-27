8d52c01a045f5ff71b73f66897d6f45ad7b20d32
LiangXiao <liangxiao0991@gmail.com>
2024-04-28 00:42:29 +0800

    fix infinite loop on banList and repeat request on some page

--- src/components/banlogTable.vue
+++ src/components/banlogTable.vue
@@ -85,7 +85,7 @@ watch([pageSize, current], () => {
   forceLoading.value = true
 })
 
-watch(() => endpointState.endpoint, refresh, { immediate: true })
+watch(() => endpointState.endpoint, refresh)
 
 const tableLoading = computed(() => {
   return forceLoading.value || loading.value || !list.value

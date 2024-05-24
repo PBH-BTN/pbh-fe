cca1c21afaaed42a035bad24218fd93fbb6b6bef
Gaojianli <admin@gaojianli.me>
2024-05-24 14:40:46 +0800

    fix ranks

--- src/service/topBan.ts
+++ src/service/topBan.ts
@@ -3,11 +3,14 @@ import { useEndpointStore } from '@/stores/endpoint'
 import urlJoin from 'url-join'
 import { getCommonHeader } from './utils'
 
-export async function getTopBan(num: number): Promise<topBanItem[]> {
+export async function getTopBan(limit: number): Promise<topBanItem[]> {
   const endpointStore = useEndpointStore()
   await endpointStore.serverAvailable
 
   const url = new URL(urlJoin(endpointStore.endpoint, 'api/bans/ranks'), location.href)
-  url.searchParams.set('num', String(num))
-  return fetch(url, { headers: getCommonHeader() }).then((res) => { endpointStore.assertResponseLogin(res); return res.json() })
+  url.searchParams.set('limit', String(limit))
+  return fetch(url, { headers: getCommonHeader() }).then((res) => {
+    endpointStore.assertResponseLogin(res)
+    return res.json()
+  })
 }

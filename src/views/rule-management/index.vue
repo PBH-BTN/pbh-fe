49f18136175950ba5658d6176a7954ada1971609
Gaojianli <admin@gaojianli.me>
2024-08-07 16:19:46 +0800

    增加icon

--- src/views/rule-management/index.vue
+++ src/views/rule-management/index.vue
@@ -1,6 +1,10 @@
 <template>
   <a-tabs default-active-key="ip" lazy-load>
-    <a-tab-pane v-for="type in blackListTypes" :key="type" :title="t('page.rule_management.' + type)">
+    <a-tab-pane v-for="type in blackListTypes" :key="type">
+      <template #title>
+        <iconList :type="type" />
+        {{ t('page.rule_management.' + type) }}
+      </template>
       <genericBlackList :type="type" />
     </a-tab-pane>
     <a-tab-pane key="subscribe">
@@ -12,10 +16,35 @@
   </a-tabs>
 </template>
 <script lang="ts" setup>
+import { Icon } from '@arco-design/web-vue';
 import subscribeManagement from './components/subscribe/index.vue'
 import genericBlackList from './components/generic/index.vue'
 import { useI18n } from 'vue-i18n'
 import type { ruleType } from '@/api/model/blacklist';
+import { IconLocation, IconStorage } from '@arco-design/web-vue/es/icon';
+import { defineComponent, h } from 'vue';
+
+
 const { t } = useI18n()
 const blackListTypes: ruleType[] = ['ip', 'port', 'asn', 'region', 'netType']
+const IconFont = Icon.addFromIconFontCn({ src: 'https:////at.alicdn.com/t/c/font_4646549_z3jqnpcw1dq.js' });
+
+
+
+const iconList = defineComponent({
+  props: {
+    type: String
+  },
+  setup(props) {
+    return () => {
+      switch (props.type) {
+        case 'ip': return h(IconFont, { type: "icon-IP" })
+        case 'port': return h(IconFont, { type: "icon-dituleiduankou" })
+        case 'asn': return h(IconStorage)
+        case 'region': return h(IconLocation)
+        case 'netType': return h(IconFont, { type: "icon-kuandai" })
+      }
+    }
+  }
+})
 </script>

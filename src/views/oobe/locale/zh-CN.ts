9bfb0bb17f01ccdd441be4a3edc88712f5f3dd6e
Ghost_chu <ghostchu@111.com>
2024-08-17 21:25:56 +0800

    完善提示信息，避免用户挠头

--- src/views/oobe/locale/zh-CN.ts
+++ src/views/oobe/locale/zh-CN.ts
@@ -11,14 +11,14 @@ export default {
 
   'page.oobe.welcome.title': '欢迎使用 PeerBanHelper!',
   'page.oobe.welcome.description':
-    'PeerbanHelper 是一个 BT 反吸血工具。通过连接到您的下载器的 WebUI，PeerBanHelper 可以使用这些信息检查并封禁恶意吸血者。在一切开始之前，让我们先进行一些简单配置。',
+    'PeerBanHelper 是一个 BT 反吸血工具。通过连接到您的下载器的 WebUI，PeerBanHelper 可以使用这些信息检查并封禁恶意吸血者。在一切开始之前，让我们先进行一些简单配置。',
   'page.oobe.setToken.title': '设置 Token',
   'page.oobe.setToken.description':
     '首先你需要设置一个 Token，这个 Token 可以保护你的 WebUI 不被恶意访问，并保护您的隐私。务必确保强度够高。一旦您成功登陆过一次 WebUI，后续浏览器就会记住 Token 并自动登录。',
   'page.oobe.setToken.generate': '随机生成',
 
   'page.oobe.addDownloader.title': '添加你的第一个下载器',
-  'page.oobe.addDownloader.description': '首先选择你的下载器类型，随后填写相关认证信息。',
+  'page.oobe.addDownloader.description': '首先选择你的下载器类型，随后填写下载器的 WebUI 的相关认证信息。请注意 URL 后面不要以 / 结尾。',
   'page.oobe.addDownloader.test': '测试下载器',
   'page.oobe.addDownloader.test.success': '测试成功',
 
